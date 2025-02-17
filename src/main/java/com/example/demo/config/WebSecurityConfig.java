7d90c45338c8cae64661b1c6c982c117e4a3e6ec
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2025-02-17 12:29:24 +0900

    :sparkles: feat: swagger API 문서 정의

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -62,9 +62,8 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                        .requestMatchers("/signup").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
-                        .requestMatchers("/sign").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/", "/signup", "/sign").permitAll() // 메인 페이지 요청 허가
+                        .requestMatchers("/api-docs/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,9 @@ import com.example.demo.dto.LoginResponseDto;
 import com.example.demo.dto.UserInfoRequestDto;
 import com.example.demo.dto.UserInfoResponseDto;
 import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +19,20 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "회원가입 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+    })
     @PostMapping("/signup")
     public UserInfoResponseDto signup(@RequestBody UserInfoRequestDto reqDto) {
         UserInfoResponseDto resDto = userService.signup(reqDto);
         return resDto;
     }
 
+    @Operation(summary = "로그인", description = "로그인 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공"),
+    })
     @PostMapping("/sign")
     public LoginResponseDto login(@RequestBody LoginRequestDto reqDto) {
         LoginResponseDto resDto = userService.login(reqDto);
